22989190b8c80977b8587a602b648bfb41325814
Matheus <matheus510.fonseca@gmail.com>
2018-07-13 15:13:03 -0300

    adding http server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Index do Redirecionamento do visualição
 
 var https = require('https');
+var http = require('http');
 var url = require('url');
 var handlers = require('./lib/handlers')
 var StringDecoder = require('string_decoder').StringDecoder;
@@ -15,6 +16,10 @@ var httpsServerOptions = {
   'key': fs.readFileSync('./https/key.pem'),
   'cert': fs.readFileSync('./https/cert.pem')
 };
+var httpServer = http.createServer(function(req,res){
+  internalServer(req, res);
+});
+
 var httpsServer = https.createServer(httpsServerOptions,function(req,res){
   internalServer(req,res);
 });
@@ -60,7 +65,9 @@ var internalServer = function(req, res){
 }
 
   // Start the HTTPS server
-  httpsServer.listen('5000',function(){
-    console.log('\x1b[42m%s\x1b[0m','listening at port: 5000')
+  httpServer.listen('5000',function(){
+    console.log('\x1b[40m%s\x1b[0m','listening at port: 5000')
   });
-
+  httpsServer.listen('5001',function(){
+    console.log('\x1b[42m%s\x1b[0m','listening at port: 5001')
+  });
