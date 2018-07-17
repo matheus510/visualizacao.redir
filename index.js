c6bf0588e62abde6e65aaf5d11f080b2f6a40597
Matheus <matheus510.fonseca@gmail.com>
2018-07-17 15:31:29 -0300

    adicionando workers de expiração

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 // Index do Redirecionamento do visualição
-
-var https = require('https');
 var http = require('http');
 var url = require('url');
 var handlers = require('./lib/handlers')
 var StringDecoder = require('string_decoder').StringDecoder;
 var fs = require('fs');
+var workers = require('./lib/workers');
 
 // Define the request router
 var router = {
@@ -13,7 +12,10 @@ var router = {
   'visualizacao': handlers.visualizacao,
   'static': handlers.static
 };
-
+////////////////////////////////////////////
+// QUEBRE O VIDRO DO HTTPS DE EMERGENCIA //
+//       Use em caso de urgencia        //
+/////////////////////////////////////////
 var httpsServerOptions = {
   'key': fs.readFileSync('./https/key.pem'),
   'cert': fs.readFileSync('./https/cert.pem')
@@ -64,14 +66,8 @@ var httpServer = http.createServer(function(req,res){
   internalServer(req, res);
 });
 
-var httpsServer = https.createServer(httpsServerOptions,function(req,res){
-  internalServer(req,res);
+// Start the HTTPS server
+httpServer.listen('5000',function(){
+  console.log('\x1b[35m%s\x1b[0m','listening at port: 5000')
+  workers.init();
 });
-
-  // Start the HTTPS server
-  httpServer.listen('5000',function(){
-    console.log('\x1b[35m%s\x1b[0m','listening at port: 5000')
-  });
-/*   httpsServer.listen('5001',function(){
-    console.log('\x1b[42m%s\x1b[0m','listening at port: 5001')
-  }); */
