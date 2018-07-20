b4ff8590f64e6606a0509b7bfbf861e7b2d774c1
Matheus <matheus510.fonseca@gmail.com>
2018-07-20 14:19:05 -0300

    language completely applied + code pattern applied correctly on all files

--- index.js
+++ index.js
@@ -1,74 +1,88 @@
+//
 // Initial file for PreviewMaker
-var http = require('http');
-var url = require('url');
-var handlers = require('./lib/handlers')
-var StringDecoder = require('string_decoder').StringDecoder;
-var fs = require('fs');
-var workers = require('./lib/workers');
+//
+
+// Main dependencies
+const http = require('http')
+const url = require('url')
+const handlers = require('./lib/handlers')
+const workers = require('./lib/workers')
+const string_decoder_module = require('string_decoder')
+
+// Set constructor as variable
+const StringDecoder = string_decoder_module.StringDecoder
 
 // Define the request router
-var router = {
+const router = {
   'redir': handlers.redir,
   'visualizacao': handlers.visualizacao,
-  'static': handlers.static
-};
+  'static': handlers.static,
+  'notFound': handlers.notFound
+}
+
 ////////////////////////////////////////////
 // BREAK THE GLASS IN CASE OF HTTPS     //
 //       Use em caso de urgencia        //
 /////////////////////////////////////////
-/* var httpsServerOptions = {
+/* let httpsServerOptions = {
   'key': fs.readFileSync('./https/key.pem'),
   'cert': fs.readFileSync('./https/cert.pem')
-}; */
+} */
 
-var internalServer = function(req, res){
+// Modularized server in case of https need
+const internalServer = function (req, res){
+
   // Parse received url
-  var parsedUrl = url.parse(req.url,true);
+  let parsedUrl = url.parse(req.url, true)
+
   // Obtain path
-  var path = parsedUrl.pathname;
-  var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+  let path = parsedUrl.pathname
+  let trimmedPath = path.replace(/^\/+|\/+$/g, '')
 
   // Get the query string as an object
-  var queryStringObj = parsedUrl.query;
+  let queryStringObj = parsedUrl.query
 
   // Get the HTTP method
-  var method = req.method.toLowerCase();
+  let method = req.method.toLowerCase()
 
   // Get the headers as an object
-  var headers = req.headers;
+  let headers = req.headers
 
   // Get the payload,if any
-  var decoder = new StringDecoder('utf-8');
-  var buffer = '';
+  let decoder = new StringDecoder('utf-8')
+  let buffer = ''
 
-  req.on('data', function(data) {
+  req.on('data',(data) => {
     buffer += decoder.write(data)
-  });
-  req.on('end', function(){
-    buffer += decoder.end();
+  })
+
+  req.on('end',() => {
+    buffer += decoder.end()
     console.log('start')
     // Construct the data object to send to the handler
-    var data = {
-      'trimmedPath' : trimmedPath,
-      'queryStringObject' : queryStringObj,
-      'method' : method,
-      'headers' : headers,
-      'payload' : buffer,
+    let data = {
+      'trimmedPath': trimmedPath,
+      'queryStringObject': queryStringObj,
+      'method': method,
+      'headers': headers,
+      'payload': buffer,
       'path': path
     }
-    var chosenHandler = trimmedPath.indexOf('static') > -1 ? 
-                        router['static'] : typeof(router[trimmedPath]) !== 'undefined' ?
-                        router[trimmedPath] : handlers.notFound;
-    chosenHandler(data, res);
-  });
+    // Determine handler to be used
+    let chosenHandler = trimmedPath.indexOf('static') > -1 ?
+      router['static'] : typeof (router[trimmedPath]) !== 'undefined' ?
+      router[trimmedPath] : handlers.notFound
+    // Invoke chosen handler  
+    chosenHandler(data, res)
+  })
 }
 
-var httpServer = http.createServer(function(req,res){
-  internalServer(req, res);
-});
+const httpServer = http.createServer((req,res) => {
+  internalServer(req, res)
+})
 
 // Start the HTTPS server
-httpServer.listen('5000',function(){
+httpServer.listen('5000',() => {
   console.log('\x1b[35m%s\x1b[0m','listening at port: 5000')
-  workers.init();
-});
+  workers.init()
+})
